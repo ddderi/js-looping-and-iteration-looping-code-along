30f5b4be83675e3aad7c4f5e04cb8e89d734c857
ddderi <maxime830@live.fr>
2022-01-07 05:59:44 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+
+function writeCards(target, eventname){
+    const newArray = []
+    for (const element of target ){
+        newArray.push(`Thank you, ` + element +`, for the wonderful ${eventname} gift!`)
+    } 
+    return newArray;
+}
+
+// function writeCards(target, eventName){
+//     const newArray = []
+//     for (let i=0;i<target.length;i++){
+//         newArray.push(`Thank you, ` + target[i] +`, for the wonderful ${eventName} gift!`)
+//     }
+//     return newArray;
+// }
+
+function countDown(n){
+ let i = n
+ while (i>=0){
+     console.log(i--)
+ }  
+}
